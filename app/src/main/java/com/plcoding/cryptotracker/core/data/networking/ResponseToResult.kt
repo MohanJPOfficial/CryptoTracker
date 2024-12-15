d3018d8877c95c6f28c00928521fc9da7d5984e7
Mohan <jpmohan007@gmail.com>
2024-12-15 19:32:42 +0530

    feat(Coin-List-Screen): Added network layer utility

--- app/src/main/java/com/plcoding/cryptotracker/core/data/networking/ResponseToResult.kt
+++ app/src/main/java/com/plcoding/cryptotracker/core/data/networking/ResponseToResult.kt
@@ -10,7 +10,7 @@ suspend inline fun <reified T> responseToResult(
     response: HttpResponse
 ): Result<T, NetworkError> {
 
-    return when(response.status.value) {
+    return when (response.status.value) {
         in 200..299 -> {
             try {
                 Result.Success(response.body<T>())
@@ -18,6 +18,7 @@ suspend inline fun <reified T> responseToResult(
                 Result.Error(NetworkError.SERIALIZATION)
             }
         }
+
         408 -> Result.Error(NetworkError.REQUEST_TIMEOUT)
         429 -> Result.Error(NetworkError.TOO_MANY_REQUESTS)
         in 500..599 -> Result.Error(NetworkError.SERVER_ERROR)
